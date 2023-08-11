6f0780d99ffd4d13da48420b2d39a3fd66ff5bcb
Kealen-Pillay <kealenpillay@gmail.com>
2023-08-12 11:44:13 +1200

    Configured routing

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,10 +1,11 @@
-import Image from 'next/image'
+import Link from "next/link";
 
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div>
                 <h1>CashCrowd</h1>
+                <Link href="/blog">Blog</Link>
             </div>
         </main>
     )
