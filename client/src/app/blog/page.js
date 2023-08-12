5f6a9966e03e93e4d7cc0db7712043e9c5a3d151
Kealen-Pillay <kealenpillay@gmail.com>
2023-08-12 22:22:40 +1200

    Styled background of app

--- client/src/app/blog/page.js
+++ client/src/app/blog/page.js
@@ -1,9 +1,8 @@
-import Link from "next/link";
 import Navbar from "@/app/Navbar";
 
-export default function Page() {
+export default function Blog() {
     return (
-        <div>
+        <div className="bg-white h-screen">
             <Navbar />
         </div>
     )
