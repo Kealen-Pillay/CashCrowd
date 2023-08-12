c8b7ab98be410d25f4067adade6be3d4ff68af60
Kealen-Pillay <kealenpillay@gmail.com>
2023-08-12 22:24:37 +1200

    Added hover colour change to nav

--- client/src/app/Navbar.js
+++ client/src/app/Navbar.js
@@ -7,15 +7,15 @@ export default function Navbar() {
                 <div className="w-4/5 text-black font-bold text-4xl pl-5">
                     <h1>CashCrowd</h1>
                 </div>
-                <div className="w-1/5 flex flex-row justify-between bg-amber-300 font-bold pr-5">
+                <div className="w-1/5 flex flex-row justify-between font-bold pr-5">
                     <div className="text-black">
-                        <Link href="/">Home</Link>
+                        <Link href="/" className="hover:text-[#FCB52C]">Home</Link>
                     </div>
                     <div className="text-black">
-                        <Link href="/blog">Crowd</Link>
+                        <Link href="/blog" className="hover:text-[#FCB52C]">Crowd</Link>
                     </div>
                     <div className="text-black">
-                        <Link href="/broadband">Broadband</Link>
+                        <Link href="/broadband" className="hover:text-[#FCB52C]">Broadband</Link>
                     </div>
                 </div>
             </div>
