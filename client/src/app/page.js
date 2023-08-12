184f9efa6f56c903a3cc1a75768bfbf9ff872348
dwalshjones <dwalshjones@icloud.com>
2023-08-13 09:56:07 +1200

    added images and update css to tailwind

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,9 +1,121 @@
-import Navbar from "@/app/Navbar";
+import Navbar from '@/app/Navbar'
+import Link from 'next/link'
 
 export default function Home() {
-    return (
-        <div className="bg-white h-screen">
-            <Navbar/>
+  return (
+    <div className="bg-white h-screen">
+      <Navbar />
+
+      <section id="showcase">
+        <div className="container mx-auto px-4">
+          <div className="top flex justify-between items-center">
+            <div className="left-block flex-1 p-4">
+              <h1 className="text-4xl font-bold">Compare NZ</h1>
+              <p>
+                Helping kiwis to find the best price, speed, quality, and
+                service.
+              </p>
+              <p className="mt-4">
+                Compare the best broadband deals in your area
+              </p>
+            </div>
+            <div className="right-block flex-1 p-4">
+              <img
+                src="./image/banner.png"
+                alt="broadband"
+                className="max-w-full h-auto"
+              />
+            </div>
+          </div>
+          <div className="middle flex justify-between items-start py-4">
+            <div className="text_information flex-1 p-2 text-center">
+              <p className="text-top font-bold">Most popular</p>
+              <h1 className="text-2xl font-semibold">NZ Broadband</h1>
+              <p className="text-bottom font-bold">this week</p>
+            </div>
+            <div className="plan_1 flex-1 p-2 m-2 max-w-md rounded-lg shadow">
+              <p className="plan_title font-bold">Fibre 100</p>
+              <h1 className="plan_price text-2xl font-semibold">$84.99</h1>
+              <h3 className="plan_features font-bold">Unlimited Data</h3>
+              <ul className="mt-4">
+                <li>100/20 Mbps</li>
+                <li>12 Months</li>
+                <li>Free Modem</li>
+                <li>Free Installation</li>
+              </ul>
+              <div className="button mt-4">
+                <Link legacyBehavior href="./broadband">
+                  <a className="px-6 py-3 text-white bg-yellow-500 rounded transition duration-300 hover:bg-gray-700">
+                    Check your address
+                  </a>
+                </Link>
+              </div>
+              <img
+                src="/image/CleanShot 2023-08-12 at 15.22.37@2x.png"
+                alt="plan_1"
+                className="max-w-full h-auto mx-auto rounded-b-lg"
+              />
+            </div>
+            <div className="plan_2 flex-1 p-2 m-2 max-w-md rounded-lg shadow">
+              <p className="plan_title font-bold">Fibre 100</p>
+              <h1 className="plan_price text-2xl font-semibold">$84.99</h1>
+              <h3 className="plan_features font-bold">Unlimited Data</h3>
+              <ul className="mt-4">
+                <li>100/20 Mbps</li>
+                <li>12 Months</li>
+                <li>Free Modem</li>
+                <li>Free Installation</li>
+              </ul>
+              <div className="button mt-4 ">
+                <Link legacyBehavior href="/broadband">
+                  <a className="px-6 py-3 text-white bg-yellow-500 rounded transition duration-300 hover:bg-gray-700">
+                    Check your address
+                  </a>
+                </Link>
+              </div>
+              <img
+                src="/image/CleanShot 2023-08-12 at 15.23.13@2x.png"
+                alt="plan_2"
+                className="max-w-full h-auto mx-auto rounded-b-lg"
+              />
+            </div>
+            <div className="plan_3 flex-1 p-2 m-2 max-w-md rounded-lg shadow">
+              <p className="plan_title font-bold">Fibre 100</p>
+              <h1 className="plan_price text-2xl font-semibold">$84.99</h1>
+              <h3 className="plan_features font-bold">Unlimited Data</h3>
+              <ul className="mt-4">
+                <li>100/20 Mbps</li>
+                <li>12 Months</li>
+                <li>Free Modem</li>
+                <li>Free Installation</li>
+              </ul>
+              <div className="button mt-4">
+                <Link legacyBehavior href="./broadband">
+                  <a className="px-6 py-3 text-white bg-yellow-500 rounded transition duration-300 hover:bg-gray-700">
+                    Check your address
+                  </a>
+                </Link>
+              </div>
+              <img
+                src="image/CleanShot 2023-08-12 at 15.23.27@2x.png"
+                alt="plan_3"
+                className="max-w-full h-auto mx-auto rounded-b-lg"
+              />
+            </div>
+            <div className="more flex-1 p-2">
+              <Link legacyBehavior href="./broadband">
+                <a className="px-6 py-3 text-white bg-yellow-500 rounded transition duration-300 hover:bg-gray-700">
+                  More
+                </a>
+              </Link>
+            </div>
+          </div>
         </div>
-    )
+      </section>
+
+      <footer className="bg-gray-800 text-white py-2 text-center">
+        <p>Web Development &copy; 2018</p>
+      </footer>
+    </div>
+  )
 }
