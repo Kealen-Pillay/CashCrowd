e472bc695ff62662a3cfa3418f5ae391c2fb65a1
Kealen-Pillay <kealenpillay@gmail.com>
2023-08-12 22:35:16 +1200

    Minor styling

--- client/src/app/blog/page.js
+++ client/src/app/blog/page.js
@@ -5,11 +5,11 @@ export default function Blog() {
         <div className="bg-white h-screen">
             <Navbar/>
             <div className="w-screen my-10 flex justify-center font-bold text-black">
-                <h3 className="px-5">Popular Discussions</h3>
-                <h3 className="px-5 text-gray-400">Recent</h3>
+                <h3 className="px-5 hover:cursor-pointer">Popular Discussions</h3>
+                <h3 className="px-5 text-gray-400 hover:cursor-pointer">Recent</h3>
             </div>
             <div className="w-screen flex justify-center">
-                <button className="bg-[#FCB52C] p-3 rounded-full font-bold hover:scale-105">Start a Discussion</button>
+                <button className="bg-[#FCB52C] p-3 rounded-full font-bold hover:scale-105 hover:cursor-pointer">Start a Discussion</button>
             </div>
         </div>
     )
