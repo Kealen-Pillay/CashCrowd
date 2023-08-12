b16504a8840524a147dde0ab5bb6cdc85cb62469
Kealen-Pillay <kealenpillay@gmail.com>
2023-08-12 22:33:49 +1200

    Added post button

--- client/src/app/blog/page.js
+++ client/src/app/blog/page.js
@@ -3,7 +3,14 @@ import Navbar from "@/app/Navbar";
 export default function Blog() {
     return (
         <div className="bg-white h-screen">
-            <Navbar />
+            <Navbar/>
+            <div className="w-screen my-10 flex justify-center font-bold text-black">
+                <h3 className="px-5">Popular Discussions</h3>
+                <h3 className="px-5 text-gray-400">Recent</h3>
+            </div>
+            <div className="w-screen flex justify-center">
+                <button className="bg-[#FCB52C] p-3 rounded-full font-bold hover:scale-105">Start a Discussion</button>
+            </div>
         </div>
     )
 }
