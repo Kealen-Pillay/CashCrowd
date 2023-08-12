2bf5d9dfa904fecca0a31e3a80db6c55f82b4264
Kealen-Pillay <kealenpillay@gmail.com>
2023-08-12 13:35:06 +1200

    Added provider route

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -1,1 +1,12 @@
 import {Router} from "express";
+import {getProviders} from "../controllers/providers/ProviderController";
+
+const router: Router = Router()
+
+/**
+ * Broadband Provider Routes
+ */
+
+router.get("/providers", getProviders)
+
+export default router
