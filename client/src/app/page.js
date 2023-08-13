977a04fe4fe3330385f8973e738aa516da3cb338
dwalshjones <dwalshjones@icloud.com>
2023-08-13 13:41:28 +1200

    tweak homepage

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -27,111 +27,101 @@ export default function Home() {
               />
             </div>
           </div>
-          <div className="middle flex flex-nowrap justify-between items-start py-4">
-            <div className="text_information flex-1 p-2 text-center">
-              <p className="text-top font-bold">Most popular</p>
-              <h1 className="text-2xl font-semibold">NZ Broadband</h1>
-              <p className="text-bottom font-bold">this week</p>
-            </div>
-            <div className="flex-auto flex-col p-2 m-2 max-w-md rounded-lg shadow h-[350px] relative">
-              <p className="plan_title font-bold">Fibre 100</p>
-              <h1 className="plan_price text-2xl font-semibold">$84.99</h1>
-              <h3 className="plan_features font-bold">Unlimited Data</h3>
-              <ul className="mt-4">
-                <li>100/20 Mbps</li>
-                <li>12 Months</li>
-                <li>Free Modem</li>
-                <li>Free Installation</li>
-              </ul>
-              <div className="button mt-4 absolute bottom-0 left-0">
-                <Link legacyBehavior href="./broadband">
-                  <a className="px-6 py-3 text-white bg-yellow-500 rounded transition duration-300 hover:bg-gray-700">
-                    Check your address
-                  </a>
-                </Link>
+
+          <div className="text_information flex-1 p-2 text-center">
+            <p className="text-top font-bold">Most popular</p>
+            <h1 className="text-2xl font-semibold">NZ Broadband</h1>
+            <p className="text-bottom font-bold">this week</p>
+          </div>
+          <div className="flex flex-col md:flex-row justify-between items-start py-4">
+            <div className="plan_1 flex justify-between flex-col p-2 m-2 max-w-md rounded-lg shadow h-[480px] relative">
+              <div>
+                <p className="plan_title font-bold">Fibre 100</p>
+                <h1 className="plan_price text-2xl font-semibold">$84.99</h1>
+                <h3 className="plan_features font-bold">Unlimited Data</h3>
+                <ul className="mt-4">
+                  <li>100/20 Mbps</li>
+                  <li>12 Months</li>
+                  <li>Free Modem</li>
+                  <li>Free Installation</li>
+                </ul>
+                <div className="button mt-4">
+                  <Link legacyBehavior href="./broadband">
+                    <a className="px-6 py-3 text-white bg-yellow-500 rounded transition duration-300 hover:bg-gray-700 whitespace-normal">
+                      Check your address
+                    </a>
+                  </Link>
+                </div>
               </div>
               <img
                 src="/image/CleanShot 2023-08-12 at 15.22.37@2x.png"
                 alt="plan_1"
-                className="absolute
-                max-w-full 
+                className="max-w-full 
                 h-auto
                 mx-auto
-                object-contain 
-                rounded-b-lg 
-                translate-y-[0.94rem] 
-                border-2 
-                border-grey-600
-                bottom-0
-                left-0"
+                rounded-b-lg"
               />
             </div>
-            <div className="plan_2 flex-auto flex-col p-2 m-2 max-w-md rounded-lg shadow h-[350px] relative">
-              <p className="plan_title font-bold">Fibre 100</p>
-              <h1 className="plan_price text-2xl font-semibold">$84.99</h1>
-              <h3 className="plan_features font-bold">Unlimited Data</h3>
-              <ul className="max-h-[100px] overflow-y-hidden mt-4">
-                <li>100/20 Mbps</li>
-                <li>12 Months</li>
-                <li>Free Modem</li>
-                <li>Free Installation</li>
-              </ul>
-              <div className="button absolute bottom-0 left-0">
-                <Link legacyBehavior href="/broadband">
-                  <a className="px-6 py-3 text-white bg-yellow-500 rounded transition duration-300 hover:bg-gray-700">
-                    Check your address
-                  </a>
-                </Link>
+
+            <div className="plan_3 flex justify-between flex-col p-2 m-2 max-w-md rounded-lg shadow h-[480px] min-w-[300px] relative">
+              <div>
+                <p className="plan_title font-bold">Fibre 100</p>
+                <h1 className="plan_price text-2xl font-semibold">$84.99</h1>
+                <h3 className="plan_features font-bold">Unlimited Data</h3>
+                <ul className="max-h-[100px] overflow-y-hidden mt-4">
+                  <li>100/20 Mbps</li>
+                  <li>12 Months</li>
+                  <li>Free Modem</li>
+                </ul>
+                <div className="button mt-4">
+                  <Link legacyBehavior href="./broadband">
+                    <a className="px-6 py-3 text-white bg-yellow-500 rounded transition duration-300 hover:bg-gray-700 whitespace-normal">
+                      Check your address
+                    </a>
+                  </Link>
+                </div>
               </div>
               <img
                 src="/image/CleanShot 2023-08-12 at 15.23.13@2x.png"
-                alt="plan_2"
-                className="absolute
+                alt="plan_3"
+                className="
                 max-w-full 
                 h-auto
                 mx-auto
-                object-contain
-                rounded-b-lg 
-                border-2 
-                border-grey-600
-                bottom-0
-                left-0"
+                rounded-b-lg"
               />
             </div>
-            <div className="plan_2 flex-auto flex-col p-2 m-2 max-w-md rounded-lg shadow h-[350px] relative">
-              <p className="plan_title font-bold">Fibre 100</p>
-              <h1 className="plan_price text-2xl font-semibold">$84.99</h1>
-              <h3 className="plan_features font-bold">Unlimited Data</h3>
-              <ul className="max-h-[100px] overflow-y-hidden mt-4">
-                <li>100/20 Mbps</li>
-                <li>12 Months</li>
-                <li>Free Modem</li>
-                <li>Free Installation</li>
-              </ul>
-              <div className="button mt-4">
-                <Link legacyBehavior href="./broadband">
-                  <a className="px-6 py-3 text-white bg-yellow-500 rounded transition duration-300 hover:bg-gray-700">
-                    Check your address
-                  </a>
-                </Link>
+
+            <div className="plan_3 flex justify-between flex-col p-2 m-2 max-w-md rounded-lg shadow h-[480px] relative">
+              <div>
+                <p className="plan_title font-bold">Fibre 100</p>
+                <h1 className="plan_price text-2xl font-semibold">$84.99</h1>
+                <h3 className="plan_features font-bold">Unlimited Data</h3>
+                <ul className="max-h-[100px] overflow-y-hidden mt-4">
+                  <li>100/20 Mbps</li>
+                  <li>12 Months</li>
+                  <li>Free Modem</li>
+                  <li>Free Installation</li>
+                </ul>
+                <div className="button mt-4">
+                  <Link legacyBehavior href="./broadband">
+                    <a className="px-6 py-3 text-white bg-yellow-500 rounded transition duration-300 hover:bg-gray-700 whitespace-normal">
+                      Check your address
+                    </a>
+                  </Link>
+                </div>
               </div>
               <img
                 src="image/CleanShot 2023-08-12 at 15.23.27@2x.jpg"
                 alt="plan_3"
-                className="absolute
+                className="
                 max-w-full 
                 h-auto
                 mx-auto
-                object-contain 
-                rounded-b-lg
-                transform scale-y-[0.74] 
-                translate-y-[1.4rem] 
-                border-2 
-                border-grey-600
-                bottom-0
-                left-0"
+                rounded-b-lg"
               />
             </div>
+
             <div className="more flex-1 p-2">
               <Link legacyBehavior href="./broadband">
                 <a className="px-6 py-3 text-white bg-yellow-500 rounded transition duration-300 hover:bg-gray-700">
